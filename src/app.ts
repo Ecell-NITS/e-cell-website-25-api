d5e88ab7fd63ae77a9afc6aaed4a01c925d9f714
Bishal Das <dasbishal1717@gmail.com>
2025-12-13 08:21:31 +0530

    prehooks fixed

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,6 @@ app.use(express.json());
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
+app.get('/version', (_req, res) => res.json({ version: '1.0.0' }));
+
 export default app;
